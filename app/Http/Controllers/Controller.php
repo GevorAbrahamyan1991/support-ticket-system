cc4aee53b2ac57cf4eb0f989534b2f87e250257c
GevorAbrahamyan1991 <abgevor@gmail.com>
2025-07-11 00:20:03 +0400

    Add role-based authorization method in Controller for user access control

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+    protected function authorizeRole(string|array $roles)
+    {
+        $user = Auth::user();
+        if (is_string($roles)) {
+            $roles = [$roles];
+        }
+        if (!$user || !in_array($user->role, $roles)) {
+            abort(403, 'Unauthorized.');
+        }
+    }
 }
